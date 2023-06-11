d82ddfb017d5df2b9278fc7a6c754eaa1e162641
farzad <farzad.ghlm8@gmail.com>
2023-06-10 19:54:44 -0600

    first commit

---
+++ password_tk.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import messagebox
+import random
+import string
+
+class PasswordGenerator:
+    def __init__(self, root):
+        self.root = root
+        self.settings = {
+            'lower': True,
+            'upper': True,
+            'number': True,
+            'symbol': True,
+            'space': False,
+            'length': 8
+        }
+        self.init_ui()
+
+    
+
+        
+
+        
+   
+        
+
+
+
+
+
